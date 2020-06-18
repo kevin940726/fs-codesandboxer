d52001d09f7c724ed75bb3247810aeec52db4b6f
Kai Hao <kai@kaihao.dev>
2020-06-18 22:16:15 +0800

    Add options to cli

--- bin/fs-codesandboxer.js
+++ bin/fs-codesandboxer.js
@@ -10,13 +10,29 @@ const cli = meow(
   chalk`
     {bold Usage}
       {bold $} {green fs-codesandboxer} {bold <source-directory-path>}
+
+    {bold Options}
+      {green.underline --ignore-paths}, {green.underline -i}	Paths to be ignored.
+      {green.underline --skip-uploading-binary-files}	Skip uploading binary files to CodeSandbox, might cause the app to not function correctly.
  
     {bold Examples}
       {bold $} {green fs-codesandboxer} ./examples/react
       CodeSandbox ID	: {green.bold 2qxth} {dim (copied to clipboard)}
       Editor URL  	: {bold.underline https://codesandbox.io/s/2qxth}
       Preview URL 	: {bold.underline https://2qxth.csb.app}
-`
+`,
+  {
+    flags: {
+      ignorePaths: {
+        type: 'string',
+        alias: 'i',
+        isMultiple: true,
+      },
+      skipUploadingBinaryFiles: {
+        type: 'boolean',
+      },
+    },
+  }
 );
 
 (async function main() {
@@ -24,7 +40,10 @@ const cli = meow(
     throw new Error('Missing source directory path.');
   }
 
-  const sandboxID = await fsCodeSandboxer(cli.input[0]);
+  const sandboxID = await fsCodeSandboxer(cli.input[0], {
+    ignorePaths: cli.flags.ignorePaths,
+    skipUploadingBinaryFiles: cli.flags.skipUploadingBinaryFiles,
+  });
 
   await clipboardy.write(sandboxID);
 
